159a8da788b274ee7460055c312aa1919aba4fea
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-08-22 20:59:46 +0100

    test passed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+   def name=(dog_name)   #setter
+        @this_dogs_name = dog_name   #instance variable
+   end
+   
+   def name #getter
+        @this_dogs_name
+   end
 end
+
+
+lassie = Dog.new #instance
+lassie.name = "Lassie"
+lassie.name
